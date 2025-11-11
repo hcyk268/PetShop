1ee8268faa12a17f37d42a8b726e6f8836a59c75
AnnaDiva <quocanhnnt@gmail.com>
2025-11-11 22:46:12 +0700

    Add 2 new folders Controls and Services
    Add NavigationBar.xaml and NavigationService
    
    Add style NavButtonStyle in ButtonStyles.xaml

---
+++ Services/NavigationService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pet_Shop_Project.Services
+{
+    public class NavigationService
+    {
+        private Frame mainFrame;
+        private static NavigationService instance;
+
+        private NavigationService() { }
+
+        public static NavigationService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NavigationService();
+                }
+                return instance;
+            }
+        }
+
+        public void Initialize(Frame frame)
+        {
+            mainFrame = frame;
+        }
+
+        public void NavigateToHome()
+        {
+            if (mainFrame != null)
+            {
+                mainFrame.Navigate(new HomePage());
+            }
+        }
+    }
+}
