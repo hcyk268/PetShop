2c37be6e0eb5dd663db3a1fd088c0a7eb1d5328e
hcyk268 <vvhipt206.contact@gmail.com>
2025-11-13 09:19:46 +0700

    fix

--- views/OrderQueuePage.xaml.cs
+++ views/OrderQueuePage.xaml.cs
@@ -1,23 +1,12 @@
-﻿using MaterialDesignColors.Recommended;
-using Pet_Shop_Project.Models;
+﻿using Pet_Shop_Project.Models;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Pet_Shop_Project.Views
 {
